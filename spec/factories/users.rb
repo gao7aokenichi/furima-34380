b8f12cd78835f24b9a58525dd921f0c7322a10fb
gao7aokenichi <kendantyo@gmail.com>
2021-03-05 19:17:23 +0900

    テストコードの実装

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,8 +2,12 @@ FactoryBot.define do
   factory :user do
     name {Faker::Name.last_name}
     email {Faker::Internet.free_email}
-    password = Faker::Internet.password(min_length: 6)
-    password {password}
+    password {"1a" + "#{Faker::Internet.password(min_length: 6)}"}
     password_confirmation {password}
+    last_name {"山田"}
+    first_name {"太郎"}
+    last_name_kana{"ヤマダ"}
+    first_name_kana{"タロウ"}
+    birthday{"1111-11-11"}
   end
 end
