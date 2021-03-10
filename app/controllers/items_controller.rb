030212602c484d0082b58d75806419f6214a2c43
gao7aokenichi <kendantyo@gmail.com>
2021-03-10 21:09:41 +0900

    商品情報編集機能２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,6 +23,9 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    if current_user != @item.user
+      redirect_to root_path
+    end
   end
 
   def update
