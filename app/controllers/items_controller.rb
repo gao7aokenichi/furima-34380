457ef500597b59bbc318ae4f5bb48ed485a9fd99
gao7aokenichi <kendantyo@gmail.com>
2021-03-16 11:06:08 +0900

    商品購入機能６

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,6 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :set_item2, only: [:edit, :update, :destroy]
-  before_action :set_item0, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -55,10 +54,6 @@ class ItemsController < ApplicationController
   end
 
   def set_item2
-    redirect_to root_path if current_user != @item.user
-  end
-
-  def set_item0
-    redirect_to root_path if @item.order.present?
+    redirect_to root_path if current_user != @item.user || @item.order.present?
   end
 end
