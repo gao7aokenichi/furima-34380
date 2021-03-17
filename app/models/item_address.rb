adbd5fc1aa1d3979b9503423c51942dde174bc5e
gao7aokenichi <kendantyo@gmail.com>
2021-03-17 15:54:20 +0900

    修正３

--- app/models/item_address.rb
+++ app/models/item_address.rb
@@ -7,8 +7,7 @@ class ItemAddress
     validates :item_id
     validates :municipality
     validates :address
-    validates :phone_number, length: { maximum: 11 }
-    validates :phone_number, numericality: { message: 'cannot be registered with mixed alphanumeric characters' }
+    validates :phone_number, numericality: { message: ' cannot be registered with mixed alphanumeric characters' }, length: { maximum: 11 }
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :area_id, numericality: { other_than: 1 }
     validates :token
