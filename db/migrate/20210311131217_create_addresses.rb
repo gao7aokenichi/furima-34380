e5ddec0a3cf13506162022fa90ef10a0b4aacf1d
gao7aokenichi <kendantyo@gmail.com>
2021-03-17 16:35:41 +0900

    修正４

--- db/migrate/20210311131217_create_addresses.rb
+++ db/migrate/20210311131217_create_addresses.rb
@@ -1,4 +1,4 @@
-class Create < ActiveRecord::Migration[6.0]
+class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
       t.string     :postal_code, null: false
